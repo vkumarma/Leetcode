2a331b87afc82f7d12c68758a2b923a3a42ca89e
vkumarma <72992409+vkumarma@users.noreply.github.com>
2022-04-11 07:06:32 -0700

    Add files via upload

---
+++ invertTree.py
@@ -0,0 +1,7 @@
+def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+  if root == None:
+      return 
+  root.left,root.right = root.right,root.left
+  self.invertTree(root.left)
+  self.invertTree(root.right)
+  return root
